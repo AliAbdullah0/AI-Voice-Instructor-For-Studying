b6acd30f1737ce10b68e7ba3179d6ae154315643
AliAbdullah0 <aliabdullah10nov2006@gmail.com>
2025-04-06 21:06:36 +0500

    ./sda

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -20,11 +20,11 @@ const Hero = () => {
         <h1 className='lg:text-7xl md:text-6xl text-4xl text-white'>For Students</h1>
         </div>
         <div className="flex gap-3 mt-4">
-            <Link href="/" className='btn-secondary px-14 py-5'>
+            <Link href="/" className='btn-secondary px-14 py-5 px-8 py-3'>
                 <p className=''>CONTACT SALES</p>
                 <Image src="/svg/dotted-arrow.png" width={10} height={10} alt="arrow" />
             </Link>
-            <Link href="/" className='btn-primary rounded-4xl text-3xl px-14 py-5 '>
+            <Link href="/" className='btn-primary rounded-4xl text-3xl md:px-14 px-8 py-3 md:py-5 '>
                 <p className='text-neutral-200 text-sm'>SIGN UP</p>
             </Link>
         </div>
