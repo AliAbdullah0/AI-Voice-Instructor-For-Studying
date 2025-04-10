c6bd01ca755e0ade3d7fae11fff8c798af88d690
AliAbdullah0 <aliabdullah10nov2006@gmail.com>
2025-04-10 20:13:42 +0500

    ...

--- app/(root)/generate/page.tsx
+++ app/(root)/generate/page.tsx
@@ -1,9 +1,11 @@
 import { getCurrentUser } from '@/actions/user.actions'
 import Agent from '@/components/Agent'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 const GenerateCourse = async () => {
     const currentUser = await getCurrentUser()
+    if(!currentUser) redirect('/sign-in')
   return (
     <Agent type='generate' username={currentUser.username} userId={currentUser.id}/>
   )
